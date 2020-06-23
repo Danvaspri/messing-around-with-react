c5ca8bf7b9217ee8fb3749d9ff19d387787085e0
Daniel Vasquez <andresvp2002@gmail.com>
2020-06-22 23:28:04 -0500

    pre-assignment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
 import './App.css';
+import Menu from './components/MenuComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { DISHES } from './shared/dishes';
 
-function App() {
-
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
     return (
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            
           </div>
+         
         </Navbar>
+        
       </div>
+       
     );
-  
+  };
 }
+  
+
 
 
 
